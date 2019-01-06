83c7d4a57c094f57735f5d9a3a887c4387d22f77
Petriu <43291831+Petriu@users.noreply.github.com>
2019-01-06 13:20:00 -0800

    While loops

--- pre jS/test.js
+++ pre jS/test.js
@@ -1,7 +1,8 @@
-var myAge = 26;
-
-if (myAge < 18 || myAge || myAge ===25){
-    document.write("you aint coming!");
-}else{
-  document.write("You can come, you cool dude");
+var age = 5;
+while(age < 10){
+    
+console.log("Your age is less than 10");
+age++;
 }
+
+document.write("You are now over 10");
