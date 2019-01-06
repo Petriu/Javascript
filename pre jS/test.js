023c7d48a99fb6d7e60ebb8d35d23cc005dfec75
Petriu <43291831+Petriu@users.noreply.github.com>
2019-01-06 12:14:51 -0800

    Else if Statements  first use.

--- pre jS/test.js
+++ pre jS/test.js
@@ -1,11 +1,10 @@
-var youLikeMeat = true;
-var myNum = 10;
-
-
-if(myNum == 10){
-    
-    document.write("you like meat");
-
-} else{
-    document.write("you hate meat");
+var myAge = 29;
+if (myAge > 30){
+    document.write("you are over 30!");
+} else if (myAge > 20){
+ document.write("you are over 30!");
+} else if (myAge>10){
+document.write("you are over 10!");
+} else {
+    document.write("you are not over 10!");
 }
