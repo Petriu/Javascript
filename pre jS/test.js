7b4ab74e68a035e5a8232dcb8ec494a34955ea33
Petriu <43291831+Petriu@users.noreply.github.com>
2019-01-06 14:07:40 -0800

    practical using loops

--- pre jS/test.js
+++ pre jS/test.js
@@ -1,16 +1,5 @@
+var links = document.getElementsByTagName("a");
 
-for (i = 0; i < 10; i++) {
-
-if(i ===5 || i ===3) {
-   
-        continue;
-
+for(i = 0; i < links.length; i++){
+    links[i].className = "link-" + i;
 }
-    console.log(i);
-    if(i ===7) {
-    break;
-    }
-
-}
-
-console.log("I have broken out of the loop");
