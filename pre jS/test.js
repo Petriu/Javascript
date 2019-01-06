d27ee5cdcd31c634976225042ba320a86491a2ed
Petriu <43291831+Petriu@users.noreply.github.com>
2019-01-06 12:07:58 -0800

    If Statements first use

--- pre jS/test.js
+++ pre jS/test.js
@@ -1,1 +1,11 @@
-alert("Hello Wordl");
+var youLikeMeat = true;
+var myNum = 10;
+
+
+if(myNum == 10){
+    
+    document.write("you like meat");
+
+} else{
+    document.write("you hate meat");
+}
