b1df69eac24b798cf7033eac59895fdb9873b479
Petriu <43291831+Petriu@users.noreply.github.com>
2019-01-06 13:33:03 -0800

    For loops we use

--- pre jS/test.js
+++ pre jS/test.js
@@ -1,8 +1,11 @@
-var age = 5;
-while(age < 10){
-    
-console.log("Your age is less than 10");
-age++;
+
+var links = document.getElementsByTagName("a");
+
+
+for (i = 1; i < links.length; i++) {
+
+console.log("this is link numer" + i);
+
 }
 
-document.write("You are now over 10");
+document.write("all links now looped");
