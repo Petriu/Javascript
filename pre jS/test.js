44af1130db51ae697a106adcd8b1af80f09da492
Petriu <43291831+Petriu@users.noreply.github.com>
2019-01-10 09:26:29 -0800

    The Date Object

--- pre jS/test.js
+++ pre jS/test.js
@@ -1,25 +1,25 @@
+var PastDate = new Date(1545, 11, 2, 10, 30, 15);
+//var myFutureDate= new Date(2515, 0 ,31, 10, 30, 15);
 
+//console.log(myPastDate);
+//console.log(myFutureDate);  
 
+var birthday = new Date (1985, 0 ,15 ,11, 15, 25);
+var birthday2 = new Date (1985, 0 ,15 ,11, 15, 25);
+console.log(birthday.getMonth());
 
-var Car = function(maxSpeed, driver) {
-    this.maxSpeed = maxSpeed;
-  this.driver = driver;
-this.drive: function(speed, time){
-    
-    console.log(speed * time); 
+console.log(birthday.getFullYear());
 
-} ;
+console.log(birthday.getDate());
 
-this.logDriver = function() {
-    console.log("driver name is" + this.driver);
-};
+console.log(birthday.getDay());
+
+console.log(birthday.getHours());
+
+console.log(birthday.getTime());
+
+if(birthday.getTime() == birthday2.getTime()){
+    console.log("birthdays are equal");
+} else {
+    console.log("birthdays are not equal");
 }
-
-var myCar = new Car (70, "Ninja Man");
-var myCar2 = new Car (40, "Humpty Dumpty");
-var myCar3 = new Car (10, "Shaun");
-var myCar4 = new Car (99, "James Bond");
-
-myCar.drive(30,5);
-myCar3.logDriver();
-
