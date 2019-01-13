ea460f058ea1fc453528c1d791b01b11cafe8c6f
Petriu <43291831+Petriu@users.noreply.github.com>
2019-01-13 11:04:21 -0800

    Click events and onload window.

--- pre jS/test.js
+++ pre jS/test.js
@@ -1,25 +1,27 @@
-var PastDate = new Date(1545, 11, 2, 10, 30, 15);
-//var myFutureDate= new Date(2515, 0 ,31, 10, 30, 15);
+function setUpEvents(){
 
-//console.log(myPastDate);
-//console.log(myFutureDate);  
 
-var birthday = new Date (1985, 0 ,15 ,11, 15, 25);
-var birthday2 = new Date (1985, 0 ,15 ,11, 15, 25);
-console.log(birthday.getMonth());
 
-console.log(birthday.getFullYear());
+var content = document.getElementById("content");
+var button = document.getElementById("show-more");
 
-console.log(birthday.getDate());
+button.onclick = function(){
 
-console.log(birthday.getDay());
+    if(content.className == "open"){
+        //shrink the box
+     content.className= "";
+     button.innerHTML = "Show More";
+    } else {
+        //expandable
+        content.className = "open";
+        button.innerHTML = "Show Less";
+    }
+};
 
-console.log(birthday.getHours());
-
-console.log(birthday.getTime());
-
-if(birthday.getTime() == birthday2.getTime()){
-    console.log("birthdays are equal");
-} else {
-    console.log("birthdays are not equal");
 }
+
+window.onload = function(){
+setUpEvents();
+
+
+};
